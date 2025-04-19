0aa11d9581e8d9a324ede2221d7546d975277796
Tsekh_Design <tsekh_design@PC171.local>
2025-04-19 15:23:34 +0300

    Добавил просмотр сгенерированных пдфок в стородж-аминке

--- storage/templates/admin.py
+++ storage/templates/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 import nested_admin
-from .models import Template, Page, Field, PageAsset
+from .models import Template, Page, Field, PageAsset, GeneratedPdf
 
 class FieldInline(nested_admin.NestedTabularInline):
     model = Field
@@ -18,3 +18,20 @@ class PageInline(nested_admin.NestedTabularInline):
 @admin.register(Template)
 class TemplateAdmin(nested_admin.NestedModelAdmin):
     inlines = [PageInline]
+    list_display = ['name', 'version', 'generated_pdf_count']
+    
+    def generated_pdf_count(self, obj):
+        return obj.generated_pdfs.count()
+    generated_pdf_count.short_description = 'Generated PDFs'
+
+@admin.register(GeneratedPdf)
+class GeneratedPdfAdmin(admin.ModelAdmin):
+    list_display = ['template', 'created_at', 'file_link']
+    list_filter = ['template', 'created_at']
+    search_fields = ['template__name', 'data']
+    readonly_fields = ['template', 'created_at', 'file_link']
+    
+    def file_link(self, obj):
+        return f'<a href="{obj.file.url}" target="_blank">{obj.file.name.split("/")[-1]}</a>'
+    file_link.short_description = 'PDF File'
+    file_link.allow_tags = True
